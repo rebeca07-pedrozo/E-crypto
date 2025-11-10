7bba2f719a5229f6df176803d6b4f52ecf953beb
Rebeca Pedrozo Cueto <rebecapedrozocueto@gmail.com>
2025-11-09 23:20:07 -0500

    Carpeta core

---
+++ core/db_connection.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "e_trading")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+def get_collection(name="cryptos"):
+    return db[name]
+
+def insert_crypto_data(data):
+    collection = get_collection()
+    if data:
+        collection.insert_many(data)
+        print(f"{len(data)} criptomonedas insertadas en MongoDB.")
+    else:
+        print("No hay datos para insertar.")
+# Este código está bajo licencia MIT.
+# (c) 2025 Rebeca Pedrozo Cueto
